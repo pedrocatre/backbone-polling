c934dfec89b0e2de48326fe8c8049afeca50a70f
pedroc <gil.catre@gmail.com>
2014-01-15 10:55:08 +0000

    Added the hability to filter data using a searchTerm in the demo application's API

--- demo/routes/process.js
+++ demo/routes/process.js
@@ -1,8 +1,22 @@
 var processesData = require('../data/processesData');
-
+var _ = require('underscore');
 /*
  * GET process listing.
  */
 exports.list = function(req, res) {
-    res.send(processesData);
+    var searchTerm = req.param('searchTerm'),
+        filteredProcessesData;
+    if(!(_.isUndefined(searchTerm))) {
+        // Filter the data using the search term
+        filteredProcessesData = _.filter(processesData, function(process) {
+            return _.some(process, function(value) {
+                // convert the value to string
+                // if the value contains the searchTerm return true
+                return ('' + value).indexOf(searchTerm) !== -1;
+            });
+        });
+        res.send(filteredProcessesData);
+    } else {
+        res.send(processesData);
+    }
 };
